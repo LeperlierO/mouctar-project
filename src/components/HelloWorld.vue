96ed8289f6335e3530fba03bafb1718e17940b88
LeperlierO <leperlierolivier3@gmail.com>
2024-01-31 14:54:31 +0100

    02 - reset project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,27 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+    <main>
+        <h1>
+            Hello World
+        </h1>
+    </main>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+<script>
+export default {
 
-h3 {
-  font-size: 1.2rem;
 }
+</script>
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+<style>
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
+    main{
+    height: 100vh;
+    width: 100vw;
+    background-color: aqua;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("hello-world", HelloWorld)
+app.mount('#app')
